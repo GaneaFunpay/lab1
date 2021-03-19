7afa1d389f6c220154f10539c1769ccd4bcc4d4a
GaneaFunpay <ganea.funpay@gmail.com>
2021-03-19 23:06:13 +0200

    hatethis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import {layout} from './layout';
+import {Layout} from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
+ <Layout>  
     <div className="App">
-      <header className="App-header">
-        <img src="https://sun9-68.userapi.com/impf/XxveXm2LPk_1KqdEibSZiZIsgZu7-AynFZunag/YprV1PhGSpw.jpg?size=720x1080&quality=96&sign=0e22aebfca17a08853bf3a15b73cab6b&type=album" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <layout/>
+      <img src="https://sun9-68.userapi.com/impf/XxveXm2LPk_1KqdEibSZiZIsgZu7-AynFZunag/YprV1PhGSpw.jpg?size=720x1080&quality=96&sign=0e22aebfca17a08853bf3a15b73cab6b&type=album" className="App-logo" alt="logo" />
     </div>
+</Layout> 
   );
 }
 
