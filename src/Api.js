c6ce84befae63af36030c7aa008cafecec2c578b
GaneaFunpay <ganea.funpay@gmail.com>
2021-06-03 13:40:51 +0300

    Api for attestation2

---
+++ src/Api.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import {GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+function Map(){
+    return(
+        <GoogleMap 
+            defaultZoom={12}
+            defaultCenter={{ lat: 47.00556 , lng: 28.51 }}
+        />
+
+    );
+}
+
+const WrappedMap= withScriptjs(withGoogleMap(Map));
+
+export const Api = () =>{
+
+        return (
+            <div style={{width: '100vw' , veight: '100vh'}} align='center' >
+                <WrappedMap 
+                    googleMapURL= "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places$key=AIzaSyDbFzYfCTxfra5bNFQ-EqnVu5C_EsJgASg"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={ <div style={{ height: `500px`, width: '500px' }} />}
+                    mapElement= {<div style={{ height: `100%` }} />}
+                />
+            </div>
+        )
+ 
+}
--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import {layout} from './layout';
 import {Layout} from 'antd';
 import 'antd/dist/antd.css';
+import {Api} from './Api.js';
 
 function App() {
   return (
@@ -11,6 +12,11 @@ function App() {
     <div className="App">
       <layout/>
       <img src="https://sun9-68.userapi.com/impf/XxveXm2LPk_1KqdEibSZiZIsgZu7-AynFZunag/YprV1PhGSpw.jpg?size=720x1080&quality=96&sign=0e22aebfca17a08853bf3a15b73cab6b&type=album" className="App-logo" alt="logo" />
+    </div>
+    <div className="App">
+     <br></br>
+     <Api/>
+      
     </div>
 </Layout> 
   );
